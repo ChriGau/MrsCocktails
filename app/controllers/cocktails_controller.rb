836c8d94a80d8094258c46ca8f2b76819a90b9e3
ChriGau <christelle.gaudron@laposte.net>
2017-08-16 16:22:56 +0200

    modified CreateCocktails

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -31,15 +31,10 @@ class CocktailsController < ApplicationController
   # POST /cocktails.json
   def create
     @cocktail = Cocktail.new(cocktail_params)
-
-    respond_to do |format|
-      if @cocktail.save!
-        format.html { redirect_to @cocktail, notice: 'Cocktail was successfully created.' }
-        format.json { render :show, status: :created, location: @cocktail }
-      else
-        format.html { render :new }
-        format.json { render json: @cocktail.errors, status: :unprocessable_entity }
-      end
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
     end
   end
 
